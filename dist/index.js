2b32d7629735e5b0a1abd12d765105d1afd9aa03
Winnie <winnie.he@gllue.com>
2021-08-09 11:17:46 +0800

    :feat: new grammar

--- dist/index.js
+++ dist/index.js
@@ -19,21 +19,21 @@ const script = `
       内有「div」。
         其「padding」「10px」。「display」「flow-root」。
           「background-color」「pink」。「font-weight」「bold」。
-        内有「label」。其「textContent」曰「重複次數」也。
+        内有「label」。其「textContent」者。曰「重複次數」也。
         有「input」。或曰「數字輸入框」。其「display」「block」。
-        有「button」。或曰「按鍵」。其「textContent」曰「更新」也。
-        有「label」。其「textContent」曰「輸入」也。
+        有「button」。或曰「按鍵」。其「textContent」者。曰「更新」也。
+        有「label」。其「textContent」者。曰「輸入」也。
         有「input」。或曰「文本框」。其「display」「block」。
   
   夫「容器」。
-    内有「pre」。或曰「顯示」。
+    内有「pre」。或曰「顯示框」。
       其「background-color」「lightgray」。「padding」「20px」。
         「white-space」「normal」。「word-break」「break-all」。
   
   夫「按鍵」。
     其「display」「block」。「margin」「10px」。
     聞「click」而答曰「
-      有彼「__INITIALIZED__」。不然。曰「吾欲「init」之。」
+      有彼「INITIALIZED」。不然。曰「吾欲「init」之。」
       吾欲「randomize」之。
     」
     聞「randomize」而答曰「
@@ -42,26 +42,29 @@ const script = `
         唸「isNaN(次數)」而得一物。
           然。曰「有數曰「0」。彼「次數」當如是。」
       唸「Math.random()」而得一物。有彼「次數」。請「*」之。
-        願「Math」「floor」之。彼「次數」當如是。
-      有文曰「當前重複次數為」。有彼「次數」。請「+」之。
+        願「Math」「floor」之。
+        彼「次數」當如是。
+      有文曰「當前重複次數為」。
+        有彼「次數」。請「+」之。
         彼「document」之「title」當如是。
-      有彼「次數」。有文曰「 x 隨機數(0-1) = 」。請「+」之。
-        有彼「次數」。請「+」之。願「logger」「notify」之。
+      有彼「次數」。有文曰「 x 隨機數(0-1) = 」。有彼「次數」。
+        請「+」之。請「+」之。
+        願「logger」「notify」之。
       有彼「次數」。願「文本框」之「value」「repeat」之。
-        彼「顯示」之「textContent」當如是。
+        彼「顯示框」之「textContent」當如是。
     」
     聞「init」而答曰「
-      有文曰「請輸入您的姓名」。願「window」「prompt」之。是為「name」。
+      有文曰「請輸入您的姓名」。願「window」「prompt」之。是為「姓名」。
       然。曰「
-        有文曰「您好」。有彼「name」。請「+」之。
+        有文曰「您好」。有彼「姓名」。請「+」之。
         願「window」「alert」之。
         夫「文本框」。
           其「background-color」「#ffaaaa」。「color」「white」。
           聞「input」而答曰「
             有彼「次數」。願吾之「value」「repeat」之。
-            彼「顯示」之「textContent」當如是。
+            彼「顯示框」之「textContent」當如是。
           」
-        有數曰「1」。彼「__INITIALIZED__」當如是。
+        有數曰「1」。彼「INITIALIZED」當如是。
       」
     」
 `;
